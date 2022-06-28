216a6734215d7fb62f0c1c863ff0c8bc0f4b77f2
juliatogr <julia.tortosa01@estudiant.upf.edu>
2022-06-28 09:26:20 +0200

    folder structure

---
+++ src/n2exercici1/N2_E1_main.java
@@ -0,0 +1,8 @@
+package n2exercici1;
+
+public class N2_E1_main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ src/n3exercici1/N3_E1_main.java
@@ -0,0 +1,10 @@
+package n3exercici1;
+
+public class N3_E1_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
