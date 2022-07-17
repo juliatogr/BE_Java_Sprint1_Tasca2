deffefa4c89e892dd347cf3a199d541979c61f11
juliatogr <julia.tortosa01@estudiant.upf.edu>
2022-07-17 19:01:25 +0200

    n2e1 finished

--- src/n2exercici1/Entrada.java
+++ src/n2exercici1/Entrada.java
@@ -1,5 +1,158 @@
 package n2exercici1;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Entrada {
 
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static byte llegirByte(String missatge) throws InputMismatchException {
+		System.out.println(missatge);
+		
+		byte answer = -1;
+		
+		boolean isValid = false;
+		
+		while (!isValid) {
+			try {
+				answer = sc.nextByte();
+				sc.nextLine();
+				isValid = true;
+			} catch (InputMismatchException ime) {
+				System.out.println("Error de format");
+				sc.nextLine();
+			}
+		}
+
+		
+		
+		return answer;
+	}
+
+	public static int llegirInt(String missatge) throws InputMismatchException {
+		System.out.println(missatge);
+		
+		int answer = -1;
+		
+		boolean isValid = false;
+		
+		while (!isValid) {
+			try {
+				answer = sc.nextInt();
+				sc.nextLine();
+				isValid = true;
+			} catch (InputMismatchException ime) {
+				System.out.println("Error de format");
+				sc.nextLine();
+			}
+		}
+		
+		return answer;
+	}
+
+	public static float llegirFloat(String missatge) throws InputMismatchException {
+		System.out.println(missatge);
+		
+		float answer = -1;
+		
+		boolean isValid = false;
+		
+		while (!isValid) {
+			try {
+				answer = sc.nextFloat();
+				sc.nextLine();				
+				isValid = true;
+			} catch (InputMismatchException ime) {
+				System.out.println("Error de format");
+				sc.nextLine();
+			}
+		}
+		
+		return answer;
+	}
+
+	public static double llegirDouble(String missatge) throws InputMismatchException {
+		System.out.println(missatge);
+		
+		double answer = -1;
+		
+		boolean isValid = false;
+		
+		while (!isValid) {
+			try {
+				answer = sc.nextDouble();
+				sc.nextLine();
+				isValid = true;
+			} catch (InputMismatchException ime) {
+				System.out.println("Error de format");
+				sc.nextLine();
+			}
+		}
+		
+		return answer;
+	}
+	
+	public static char llegirChar(String missatge) throws Exception {
+		System.out.println(missatge);
+		
+		char answer = ' ';
+		
+		boolean isValid = false;
+		
+		while (!isValid) {
+			try {
+				answer = sc.nextLine().charAt(0);
+				isValid = true;
+			} catch (Exception e) {
+				System.out.println("Error de format");
+				//sc.nextLine();
+			}
+		}
+		
+		return answer;
+	}
+
+	public static String llegirString(String missatge) throws Exception {
+		System.out.println(missatge);
+		
+		String answer = "";
+		
+		boolean isValid = false;
+		
+		while (!isValid) {
+			try {
+				answer = sc.nextLine();
+				isValid = true;
+			} catch (Exception e) {
+				System.out.println("Error de format");
+				sc.nextLine();
+			}
+		}
+		
+		return answer;
+	}
+
+	public static boolean llegirSiNo(String missatge) throws Exception {
+		// Si l’usuari/ària introdueix “s”, retorna “true”, si l’usuari introdueix “n”, retorna “false”.
+
+		boolean isYes = false;
+		
+		boolean isYesOrNo = false;
+		char answer = ' ';
+		
+		while (!isYesOrNo) {
+			
+			answer = llegirChar(missatge);
+			isYesOrNo = answer == 's' || answer == 'n';
+			
+			if (!isYesOrNo) {
+				System.out.println("El caràcter introduït no és 's' o 'n'.");
+			}
+		}
+		
+		if (isYesOrNo) isYes = answer == 's';
+		
+		return isYes;
+	}
 }
--- src/n2exercici1/N2_E1_main.java
+++ src/n2exercici1/N2_E1_main.java
@@ -1,8 +1,28 @@
 package n2exercici1;
 
 public class N2_E1_main {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		
+		System.out.println("Has indroduït: " +
+				Entrada.llegirByte("Introdueix un número enter entre -128 i 127"));
+		
+		System.out.println("Has indroduït: " +
+				Entrada.llegirInt("Introdueix un número enter"));
+		
+		System.out.println("Has indroduït: " +
+				Entrada.llegirFloat("Introdueix un número enter o decimal"));
+		
+		System.out.println("Has indroduït: " +
+				Entrada.llegirDouble("Introdueix un número enter o decimal"));
+		
+		System.out.println("Has indroduït: " +
+				Entrada.llegirChar("Introdueix un caràcter"));
+		
+		System.out.println("Has indroduït: " +
+				Entrada.llegirString("Introdueix una frase"));
+		
+		System.out.println("Has indroduït: " +
+				Entrada.llegirSiNo("Si ('s') o no ('n')? "));
 
 	}
 }
